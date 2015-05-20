0dc3428a926f66bb7937584e2c1a04d23a9b4e07
z3nRob <rhink@zendesk.com>
2015-05-20 16:57:31 +0100

    added filter for tickets only
    added filter for tickets only

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
       requests: { 
             getSearchResults: function(query){
                 return {
-                    url: "/api/v2/search.json?query=" + query,
+                    url: "/api/v2/search.json?query=type:ticket " + query,
                     type: "GET",
                     dataType: "json"
                 };
@@ -29,7 +29,6 @@
                   //do something here to submit the api call and retrieve results
                   this.ajax('getSearchResults', this.$("input#zQuery")[0].value)
                     .done(function(data) {
-                      data.sort(function(a,b) {return (a.result_type > b.result_type) ? 1 : ((b.result_type > a.result_type) ? -1 : 0);} );
                       console.dir(data);
                       //send data to server
                       //create file based on configuration
