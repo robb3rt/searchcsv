668c567059124b31fb5ddd2223ae1ea2dae3025d
robb3rt <nrgrobbert@msn.com>
2015-05-20 22:09:18 +0100

    added uploading functionality
    added uploading functionality to my php script

--- app.js
+++ app.js
@@ -8,11 +8,26 @@
                     type: "GET",
                     dataType: "json"
                 };
-            }
+            },
+			
+			getSearchPageResults: function(query, x){
+                return {
+                    url: "/api/v2/search.json?page=" + x + "&query=type:ticket " + query,
+                    type: "GET",
+                    dataType: "json"
+                };
+            },
+			requestCSV: function(data){
+				return {
+					url: "https://www.robberthink.com/createcsv.php",
+					type: "POST",
+					data: {data : data},
+					cors: true
+				}
+			}
       },
       
       events: {
-        // Lifecycle
         'keydown #zForm': 'cancelSubmission',
         'keyup #zQuery': 'submitForm'
       },
@@ -26,13 +41,23 @@
       submitForm: function(e) {
           if (e.keyCode == 13){
               if(this.$("input#zQuery")[0].value){
-                  //do something here to submit the api call and retrieve results
+				  //TODO change layout to show that you're currently uploading the data to create a downloadable csv file.
                   this.ajax('getSearchResults', this.$("input#zQuery")[0].value)
                     .done(function(data) {
-                      console.dir(data);
-                      //send data to server
-                      //create file based on configuration
-                      //wait for server to create file
+						var size = Math.ceil(data.count/100);
+						var results = new Array(size-2);
+						results.push(data.results);
+						for (var i = 1; i < size; i++){
+							this.ajax('getSearchPageResults', this.$("input#zQuery")[0].value, i+1)
+								.done(function(restdata){
+									results.push(restdata.results);
+								});
+						}
+						this.ajax('requestCSV', results)
+							.done(function(data) {
+								//TODO create CSV file based on settings
+								console.dir(data);
+							});
                   });
               }
           }
