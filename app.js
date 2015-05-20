2f220ce41bc5c3f3a8749ad6cbf86316730b3d9c
z3nRob <rhink@zendesk.com>
2015-05-20 15:41:51 +0100

    added search bar
    added the actual search bar

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 (function() {
 
   return {
-    events: {
-      'app.activated':'doSomething'
-    },
-
-    doSomething: function() {
-    }
+      requests: { 
+            getSearchResults: function(query){
+                return {
+                    url: "/api/v2/search.json?query=" + query,
+                    type: "GET",
+                    dataType: "json"
+                };
+            }
+      },
+      
+      events: {
+        // Lifecycle
+        'keydown #zForm': 'cancelSubmission',
+        'keyup #zQuery': 'submitForm'
+      },
+      
+      cancelSubmission: function(e) {
+          if (e.keyCode == 13){
+              e.preventDefault();
+          }
+      },
+      
+      submitForm: function(e) {
+          if (e.keyCode == 13){
+              if(this.$("input#zQuery")[0].value){
+                  //do something here to submit the api call and retrieve results
+                  this.ajax('getTotalTickets', this.$("input#zQuery")[0].value)
+                    .done(function(data) {
+                      console.dir(data);
+                  }
+              }
+          }
+      }
   };
-
+    
 }());
