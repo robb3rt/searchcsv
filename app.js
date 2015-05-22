f4b8151c6338fb1882ecdb8d566fa0297c7d4f15
z3nRob <rhink@zendesk.com>
2015-05-22 17:30:11 +0100

    updated
    updated

--- app.js
+++ app.js
@@ -32,21 +32,22 @@
 					url: "/api/v2/ticket_fields.json",
 					type: "GET",
 					dataType: "json"
-				}
+				};
 			},
 			getTicketFieldsByPage: function(x){
 				return {
 					url: "/api/v2/ticket_fields.json?page=" + x,
 					type: "GET",
 					dataType: "json"
-				}
+				};
 			}
       },
       
       events: {
         'keydown #zForm': 'cancelSubmission',
         'keyup #zQuery': 'submitForm',
-        'app.activated': 'maintainLayout'
+        'app.activated': 'maintainLayout',
+        'onmousedown.sortable': 'dragSortable'
       },
       
       cancelSubmission: function(e) {
@@ -54,7 +55,6 @@
               e.preventDefault();
           }
       },
-      
       submitForm: function(e) {
           if (e.keyCode == 13){
               if(this.$("input#zQuery")[0].value){
@@ -62,12 +62,10 @@
                   this.switchTo('loading');
                   this.ajax('getSearchResults', query)
                     .done(function(data) {
-						if (data.count == 0){this.switchTo('searchbar');return;}
 						var size = Math.ceil(data.count/100);
 						var results = (size == 1) ? new Array(0) : new Array(size-2);
 						results.push(data.results);
 						for (var i = 1; i < size; i++){
-							this.ajax('getSearchResultsByPage', query, i+1)
 								.done(function(restdata){
 									results.push(restdata.results);
 								});
@@ -75,48 +73,30 @@
 						this.ajax('requestCSV', results)
 							.done(function(data) {
 								//TODO create CSV file based on settings
-								console.dir(data);
                                 this.switchTo('searchbar');
 							});
                   });
               }
           }
       },
-	  
       maintainLayout: function(e){
           if(this.currentLocation() == "nav_bar"){
-			  this.ajax('getTicketFields')
-                    .done(function(data) {
-						if (data.count == 0){this.switchTo('navbar');return;}
 						var size = Math.ceil(data.count/100);
 						var results = (size == 1) ? new Array(0) : new Array(size-2);
 						results.push(data.ticket_fields);
 						for (var i = 1; i < size; i++){
-							this.ajax('getTicketFieldsByPage', i+1)
 								.done(function(restdata){
 									results.push(restdata.ticket_fields);
 								});
 						}
-						
 						var fields = new Array();
-						for(var i = 0; i < results.length; i++ ){
-							for(var j = 0; j < results[i].length; j++){
-								var id = results[i][j].id;
-								var title = results[i][j].title;
 								fields.push({id: id, title: title});
 							}
-							
-							/*for (var a in results[i]){
-								console.dir(a.id);
-								//fields.push({id: results[i][x].id, title: results[i][x].title});
-							}*/
 						}
-						console.dir(fields);
 						this.switchTo('settings', {
 							ticket_fields: fields
 						});
 					});
-			  //get available ticket fields
           } else {
               this.switchTo('searchbar');
           }
